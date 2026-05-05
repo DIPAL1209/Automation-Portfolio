acafee4318d2dc16164c94426f70668398063611
Dipal Patel <dipal.optimitylogics@gmail.com>
2026-05-05 16:22:00 +0530

    added name field from:

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -34,7 +34,7 @@ export default function Contact() {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({...form,
-                memberName: "Dipal Patel"}),
+                    From: "Dipal Patel"}),
             })
 
             const data = await res.json()
